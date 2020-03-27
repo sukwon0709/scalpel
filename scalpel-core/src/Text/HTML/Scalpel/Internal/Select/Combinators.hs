f30bbdbbf5b6b7a98f9fd3b8722d0c1b313662dd
Sukwon Oh <sukwon0709@gmail.com>
2020-03-27 15:59:34 -0400

    added hasAttribute combinator

--- scalpel-core/src/Text/HTML/Scalpel/Internal/Select/Combinators.hs
+++ scalpel-core/src/Text/HTML/Scalpel/Internal/Select/Combinators.hs
@@ -2,16 +2,18 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# OPTIONS_HADDOCK hide #-}
-module Text.HTML.Scalpel.Internal.Select.Combinators (
-    (//)
-,   (@:)
-,   (@=)
-,   (@=~)
-,   atDepth
-,   hasClass
-,   match
-,   notP
-) where
+module Text.HTML.Scalpel.Internal.Select.Combinators
+  ( (//)
+  , (@:)
+  , (@=)
+  , (@=~)
+  , atDepth
+  , hasAttribute
+  , hasClass
+  , match
+  , notP
+  )
+where
 
 import Text.HTML.Scalpel.Internal.Select.Types
 
@@ -33,18 +35,15 @@ infixl 9 @:
 -- multiple classes, you should use the 'hasClass' utility function.
 (@=) :: AttributeName -> String -> AttributePredicate
 (@=) key value = anyAttrPredicate $ \(attrKey, attrValue) ->
-                                      matchKey key attrKey
-                                      && TagSoup.fromString value == attrValue
+  matchKey key attrKey && TagSoup.fromString value == attrValue
 infixl 6 @=
 
 -- | The '@=~' operator creates an 'AttributePredicate' that will match
 -- attributes with the given name and whose value matches the given regular
 -- expression.
-(@=~) :: RE.RegexLike re String
-      => AttributeName -> re -> AttributePredicate
+(@=~) :: RE.RegexLike re String => AttributeName -> re -> AttributePredicate
 (@=~) key re = anyAttrPredicate $ \(attrKey, attrValue) ->
-       matchKey key attrKey
-    && RE.matchTest re (TagSoup.toString attrValue)
+  matchKey key attrKey && RE.matchTest re (TagSoup.toString attrValue)
 infixl 6 @=~
 
 -- | The 'atDepth' operator constrains a 'Selector' to only match when it is at
@@ -54,11 +53,11 @@ infixl 6 @=~
 -- anchor tags that are direct children of a div tag.
 atDepth :: Selector -> Int -> Selector
 atDepth (MkSelector xs) depth = MkSelector (addDepth xs)
-  where addDepth []                 = []
-        addDepth [(node, settings)] = [
-            (node, settings { selectSettingsDepth = Just depth })
-          ]
-        addDepth (x : xs)           = x : addDepth xs
+ where
+  addDepth [] = []
+  addDepth [(node, settings)] =
+    [(node, settings { selectSettingsDepth = Just depth })]
+  addDepth (x : xs) = x : addDepth xs
 infixl 6 `atDepth`
 
 -- | The '//' operator creates an 'Selector' by nesting one 'Selector' in
@@ -66,22 +65,33 @@ infixl 6 `atDepth`
 -- anchor tags that are nested arbitrarily deep within a div tag.
 (//) :: Selector -> Selector -> Selector
 (//) a b = MkSelector (as ++ bs)
-    where (MkSelector as) = a
-          (MkSelector bs) = b
+ where
+  (MkSelector as) = a
+  (MkSelector bs) = b
 infixl 5 //
+
+-- | The 'hasAttribute' function will return true if the @attribute@ @attr is found.
+hasAttribute :: String -> AttributePredicate
+hasAttribute attr = anyAttrPredicate hasAttribute'
+ where
+  hasAttribute' (attrName, _)
+    | textAttr == TagSoup.toString attrName = True
+    | otherwise                             = False
+    where textAttr = TagSoup.castString attr
 
 -- | The classes of a tag are defined in HTML as a space separated list given by
 -- the @class@ attribute. The 'hasClass' function will match a @class@ attribute
 -- if the given class appears anywhere in the space separated list of classes.
 hasClass :: String -> AttributePredicate
 hasClass clazz = anyAttrPredicate hasClass'
-    where
-        hasClass' (attrName, classes)
-            | "class" == TagSoup.toString attrName = textClass `elem` classList
-            | otherwise                            = False
-            where textClass   = TagSoup.castString clazz
-                  textClasses = TagSoup.castString classes
-                  classList   = T.split (== ' ') textClasses
+ where
+  hasClass' (attrName, classes)
+    | "class" == TagSoup.toString attrName = textClass `elem` classList
+    | otherwise                            = False
+   where
+    textClass   = TagSoup.castString clazz
+    textClasses = TagSoup.castString classes
+    classList   = T.split (== ' ') textClasses
 
 -- | Negates an 'AttributePredicate'.
 notP :: AttributePredicate -> AttributePredicate
@@ -93,4 +103,4 @@ notP (MkAttributePredicate p) = MkAttributePredicate $ not . p
 -- attribute satisfies the predicate.
 match :: (String -> String -> Bool) -> AttributePredicate
 match f = anyAttrPredicate $ \(attrKey, attrValue) ->
-              f (TagSoup.toString attrKey) (TagSoup.toString attrValue)
+  f (TagSoup.toString attrKey) (TagSoup.toString attrValue)
